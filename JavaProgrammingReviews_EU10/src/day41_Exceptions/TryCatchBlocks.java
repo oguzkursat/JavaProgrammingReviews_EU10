33e4704fb44b2e32f7ac930cc9da17dbb69be946
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2022-12-21 00:12:40 +0300

    TryCatchBlocks test2 created

--- JavaProgrammingReviews_EU10/src/day41_Exceptions/TryCatchBlocks.java
+++ JavaProgrammingReviews_EU10/src/day41_Exceptions/TryCatchBlocks.java
@@ -22,6 +22,21 @@ public class TryCatchBlocks {
 
         System.out.println("Test completed");
 
+        System.out.println("---------------------------------------------------------");
+
+        System.out.println("Test 2 started");
+
+        int [] numbers = {1,2,3,4,5};
+
+        try {
+            System.out.println(numbers[200]);
+            System.out.println("Try block");
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Catch block");
+            System.out.println("Exception was caught");
+        }
+
+        System.out.println("Test 2 completed");
 
 
     }
