23ca89d65be8601cbd1abb38082a124558c806a8
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2022-12-21 01:08:02 +0300

    TryCatchBlocks FileInputStream created

--- JavaProgrammingReviews_EU10/src/day41_Exceptions/TryCatchBlocks.java
+++ JavaProgrammingReviews_EU10/src/day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,8 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
     public static void main(String[] args) {
 
@@ -65,6 +68,14 @@ public class TryCatchBlocks {
         }
 
         System.out.println("Cydeo");
+
+        System.out.println("---------------------------------------------------------");
+
+        try {
+            FileInputStream file = new FileInputStream("File path");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
     }
