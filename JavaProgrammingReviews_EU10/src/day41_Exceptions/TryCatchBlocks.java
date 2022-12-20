45d010f643d5d75af98a5b19a37d8935fe55e185
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2022-12-21 00:08:32 +0300

    TryCatchBlocks created

--- JavaProgrammingReviews_EU10/src/day41_Exceptions/TryCatchBlocks.java
+++ JavaProgrammingReviews_EU10/src/day41_Exceptions/TryCatchBlocks.java
@@ -5,7 +5,20 @@ public class TryCatchBlocks {
 
         System.out.println("Test started");
 
-        System.out.println(9/0);
+        try {
+
+            System.out.println(9/0);
+
+            System.out.println("Try block");
+
+        }catch (ArithmeticException e){
+
+            System.out.println("Catch block");
+
+            System.out.println("Arithmetic Exception was occured");
+
+        }
+
 
         System.out.println("Test completed");
 
