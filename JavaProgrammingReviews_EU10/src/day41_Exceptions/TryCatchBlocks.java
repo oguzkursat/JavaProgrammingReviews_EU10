d70a6beb161af4c2cfd43adb5af026ba92cf29e2
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2022-12-21 00:05:43 +0300

    TryCatchBlocks

---
+++ JavaProgrammingReviews_EU10/src/day41_Exceptions/TryCatchBlocks.java
@@ -0,0 +1,15 @@
+package day41_Exceptions;
+
+public class TryCatchBlocks {
+    public static void main(String[] args) {
+
+        System.out.println("Test started");
+
+        System.out.println(9/0);
+
+        System.out.println("Test completed");
+
+
+
+    }
+}
