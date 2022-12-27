7a0fc19973eb501363beb7607dccefbd318ef739
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2022-12-27 18:57:01 +0300

    Day 43: Abstract Class Audi,Honda and Tesla

--- JavaProgrammingReviews_EU10/src/day43_Abstraction/Car/Honda.java
+++ JavaProgrammingReviews_EU10/src/day43_Abstraction/Car/Honda.java
@@ -2,7 +2,7 @@ package day43_Abstraction.Car;
 
 import java.util.SortedMap;
 
-public class Honda extends Car {
+public final class Honda extends Car {
 
     public Honda(String brand, String model, String color, int year, double price) {
         super("Honda", model, color, year, price);
