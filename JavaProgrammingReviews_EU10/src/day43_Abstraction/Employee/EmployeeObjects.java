048235b24832be8af694a0fb2cd5afefd6bb0df6
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2022-12-27 19:58:00 +0300

    Day 43: Employee Task object Class

---
+++ JavaProgrammingReviews_EU10/src/day43_Abstraction/Employee/EmployeeObjects.java
@@ -0,0 +1,21 @@
+package day43_Abstraction.Employee;
+
+import day43_Abstraction.Car.Tesla;
+import hatalikodlar.Test;
+
+public class EmployeeObjects {
+    public static void main(String[] args) {
+
+        Tester tester = new Tester("Ali",30,'M',42,"SDET",145000.00);
+        Developer developer = new Developer("Alex",28,'M',32,"Web Developer",155000.00);
+        Teacher teacher = new Teacher("Angel",28,'F',22,"English Teacher",85000.00);
+        Driver driver = new Driver("John",34,'M',42,"Lyft Driver",75000.00);
+
+        System.out.println(tester);
+        System.out.println(developer);
+        System.out.println(teacher);
+        System.out.println(driver);
+
+
+    }
+}
