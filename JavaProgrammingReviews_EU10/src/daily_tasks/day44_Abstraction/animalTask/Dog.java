c254f3d1342754a93fa2380b70d10aad2f533158
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-02 12:58:20 +0300

    day44 Dog Class created

---
+++ JavaProgrammingReviews_EU10/src/daily_tasks/day44_Abstraction/animalTask/Dog.java
@@ -0,0 +1,20 @@
+package daily_tasks.day44_Abstraction.animalTask;
+
+public class Dog extends Animal{
+
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    public void bark(){
+        System.out.println(getName() + " is barking.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " eats pizza.");
+    }
+
+
+
+}
