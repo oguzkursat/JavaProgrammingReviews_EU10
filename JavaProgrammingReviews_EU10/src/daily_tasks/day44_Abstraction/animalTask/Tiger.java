9f8436959e02578a0bd2dfc8f043f08c111ce230
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-03 00:12:06 +0300

    day44 Tiger Class created

---
+++ JavaProgrammingReviews_EU10/src/daily_tasks/day44_Abstraction/animalTask/Tiger.java
@@ -0,0 +1,18 @@
+package daily_tasks.day44_Abstraction.animalTask;
+
+public class Tiger extends Animal implements WildAnimal{
+
+    public Tiger(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating deer");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println(getName() + " hunts deer");
+    }
+}
