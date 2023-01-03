149e7191bd17caa029a793478a78345218e0a228
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-03 12:05:22 +0300

    day44 Eagle Class updated

--- JavaProgrammingReviews_EU10/src/daily_tasks/day44_Abstraction/animalTask/Eagle.java
+++ JavaProgrammingReviews_EU10/src/daily_tasks/day44_Abstraction/animalTask/Eagle.java
@@ -1,6 +1,6 @@
 package daily_tasks.day44_Abstraction.animalTask;
 
-public class Eagle extends Animal implements WildAnimal{
+public class Eagle extends Animal implements WildAnimal,Flyable{
 
     public Eagle(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -13,5 +13,10 @@ public class Eagle extends Animal implements WildAnimal{
 
     public void hunt(){
         System.out.println(getName() + " is hunting snake");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName() + " is flying to New Mexico");
     }
 }
